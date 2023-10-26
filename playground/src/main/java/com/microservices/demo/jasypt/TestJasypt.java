45024a30ce1180c486d4a3e1308dfcfde70f48cd
taras.chmeruk@gameloft.com <taras.chmeruk@gameloft.com>
2023-10-26 14:15:22 +0300

    5

---
+++ playground/src/main/java/com/microservices/demo/jasypt/TestJasypt.java
@@ -0,0 +1,21 @@
+package com.microservices.demo.jasypt;
+
+import org.jasypt.encryption.pbe.StandardPBEBigDecimalEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.iv.RandomIvGenerator;
+
+public class TestJasypt {
+
+    public static void main (String[] args) {
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor ();
+        encryptor.setPassword("123");
+        encryptor.setAlgorithm("PBEWithHMACSHA512AndAES_256");
+        encryptor.setIvGenerator (new RandomIvGenerator ());
+
+        String encryptedText = encryptor.encrypt("test");
+        String plainText = encryptor.decrypt(encryptedText);
+        System.out.println (encryptedText);
+        System.out.println (plainText);
+
+    }
+}

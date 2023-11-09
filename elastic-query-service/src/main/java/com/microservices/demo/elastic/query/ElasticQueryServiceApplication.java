219fd5b62f84a19cd65ca695190ed31aebe64a53
taras.chmeruk@gameloft.com <taras.chmeruk@gameloft.com>
2023-11-09 15:40:57 +0200

    5

--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/ElasticQueryServiceApplication.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/ElasticQueryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.microservices.demo.elastic.query.service;
+package com.microservices.demo.elastic.query;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
